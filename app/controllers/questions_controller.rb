c5f62d414a2c71b16ce34defdfeb36cb070dad3d
Simon Reibel <simon.reibel@yahoo.fr>
2020-06-01 13:52:40 +0200

    2e

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+    def ask
+    end
+
+    def answer
+      if params[:question] == "I am going to work"
+        @reponse =  "Great!"
+      elsif params[:question].include? "?"
+        @reponse =  "Silly question, get dressed and go to work!"
+      else
+       @reponse =  "I don't care, get dressed and go to work!"
+      end
+    end
+end
